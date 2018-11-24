884d2ba85a8770ed34081369c4744c13dfb09d50
ivanzinho09 <38228885+ivanzinho09@users.noreply.github.com>
2018-11-24 22:32:57 +1030

    Update Example 2 - Inbox&Gas.sol

--- Bootcamp - Code (Commented)/Example 2 - Inbox&Gas.sol
+++ Bootcamp - Code (Commented)/Example 2 - Inbox&Gas.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.4.17; //Specifed version of solidity inside this file
 contract Inbox{
     
     //Declares a string type variable named 'message', and sets its type to public, allowing it to be utilised by different "classes"
-    //The value asigned to message as a storage variable will be stored for ALL ETERNITY witin the Ethereum blockchain
-        /*This is in contrast to local variables local variables are kind of one time created when a contract
-        is executed and then thrown away at the very end.
-        And they are never actually persisted on the block chain in this course for the most part.
-        We will be making use of storage variables but in some cases we'll be using local variables instead.*/
-    string public message;
+   	//The value asigned to message as a storage variable will be stored for ALL ETERNITY witin the Ethereum blockchain
+	    /*This is in contrast to local variables local variables are kind of one time created when a contract
+		is executed and then thrown away at the very end.
+		And they are never actually persisted on the block chain in this course for the most part.
+		We will be making use of storage variables but in some cases we'll be using local variables instead.*/
+	string public message;
     
     //Defines different functions that will be members of this contract
    
@@ -24,11 +24,16 @@ contract Inbox{
         message = newMessage;
     }
 
+    //Reviewing function based upon amount of gas needed to run
+    //https://github.com/djrtwo/evm-opcode-gas-costs/blob/master/opcode-gas-costs_EIP-150_revision-1e18248_2017-04-12.csv
 
-    //Beacuse we arent modifying the message variable (only accessing) --> using view keyword
-    //Returns specifies the return value expected to be 'returned' from a function --> only used in constant and view functions
-    //Hence, a functiont hat modified the state of the contract (eg. setMessage), will not return any values
-    function getMessage() public view returns (string){
-        return message;
+    function doMath(int a, int b){
+        a+b;
+        b-a;
+        a*b;
+        a=0;
+
     }
+
+
 }
